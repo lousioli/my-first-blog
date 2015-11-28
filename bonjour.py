216f5b557d8855a753e1ad48d240402bc9f97853
corine <corine.mangalaza@gmail.com>
2015-11-28 15:24:40 +0100

    installe

---
+++ bonjour.py
@@ -0,0 +1,12 @@
+print ("hello world")
+liste=[3,6,0,7,"pomme",42]
+print (liste[1])
+liste[1]="bonjour"
+liste.append(8)
+print (liste)
+print (len (liste))
+def carre(x):
+	return x*x
+print (carre (4))
+voyage={'pays':'madagascar','sejour':'rencontres'}
+print (voyage['pays'])
